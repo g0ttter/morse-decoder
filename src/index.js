10e54ca35ee6a56d8445a28dac4297018e6c84f9
g0ttter <kyka32@gmail.com>
2023-02-25 22:16:33 +0300

    add decoder function

--- src/index.js
+++ src/index.js
@@ -35,10 +35,35 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '99':  ' ',
 };
 
 function decode(expr) {
+    let currentSymbol = '';
+    let morseSymbol = '';
+    let result = '';
     // write your solution here
+    for(let i = 0; i < expr.length; i++) {
+        currentSymbol += expr[i];
+        //каждую десятку расшифровываем символ
+        if(i % 10 === 9 && i > 0) {
+            if(currentSymbol.indexOf('*') < 0) {
+                //начинаем расшифровывать символ
+                for(let j = 0; j < currentSymbol.length; j += 2) {
+                    if(currentSymbol[j] + currentSymbol[j + 1] == 11)
+                        morseSymbol += '-';
+                    else if(currentSymbol[j] + currentSymbol[j + 1] == 10)
+                        morseSymbol += '.';
+                }
+            }
+            else
+                morseSymbol = '99';
+            result += MORSE_TABLE[morseSymbol];
+            currentSymbol = '';
+            morseSymbol = '';
+        }
+    }
+    return result;
 }
 
 module.exports = {
